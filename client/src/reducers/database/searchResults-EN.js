44363fa71e8a71eed06d96902b6726c563f5c6b7
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-12 12:23:05 -0700

    structure defined, working on skills

---
+++ client/src/reducers/database/searchResults-EN.js
@@ -0,0 +1,5 @@
+import skills from "./skills";
+
+export default [
+	{ skills }, "experience", "projects", "about", "education", "interests", "awards", "references", "social media"
+]
---
+++ client/src/reducers/database/skills.js
@@ -0,0 +1,221 @@
+export default {
+  about: "About" + this.results.length + "skills (0.37 century)",
+  results: [
+    {
+      title: "HTML5",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "CSS3",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Javascript",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "jQuery",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Node",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Express",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "React",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Redux",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "JSX",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Angular",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Typescript",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "SCSS",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Sass",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Bootstrap",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Materialize",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "SQL",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "PostreSQL",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "NoSQL",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Mongo db",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "JSON",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "XML",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "XSLT",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Akamai",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Restful APIs",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "SPA",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "GIT",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "WordPress",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Command Line",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "NPM",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Agile",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Github",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Source Tree",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Jira",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Bitbucket",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "Confluence",
+      url: "",
+			description: "",
+			category: "Developer"
+    },
+    {
+      title: "email",
+      url: "",
+			description: "",
+			category: "Developer"
+    }
+  ],
+};
