0668df87dc133da14648c4679b11a2335105500e
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-12 14:20:09 -0700

    completing skills search results in database

--- client/src/reducers/database/skills.js
+++ client/src/reducers/database/skills.js
@@ -3,219 +3,304 @@ export default {
   results: [
     {
       title: "HTML5",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/HTML5",
+      description:
+        "HTML 5 (formerly spelled HTML5) is a markup language used for structuring and presenting content on the World Wide Web. It is the fifth and current major ...",
+      category: "Developer"
     },
     {
       title: "CSS3",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://developer.mozilla.org/en/docs/Web/CSS/CSS3",
+      description:
+        "CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.1. It brings a lot of long-awaited novelties, like rounded corners, shadows, gradients, transitions or animations, as well as new layouts like multi-columns, flexible box or grid layouts.",
+      category: "Developer"
     },
     {
       title: "Javascript",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/JavaScript",
+      description:
+        "JavaScript often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, ...",
+      category: "Developer"
     },
     {
       title: "jQuery",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/JQuery",
+      description:
+        "jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. It is free, open-source software using the permissive MIT ...",
+      category: "Developer"
     },
     {
       title: "Node",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://nodejs.org/",
+      description:
+        "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. JS Interactive on October 10-12, 2018 in Vancouver, Canada. August 2018 security ...",
+      category: "Developer"
     },
     {
       title: "Express",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://expressjs.com/",
+      description:
+        "Fast, unopinionated, minimalist web framework for Node.js ... Express is a minimal and flexible Node.js web application framework that provides a robust set of ...",
+      category: "Developer"
     },
     {
       title: "React",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://reactjs.org/",
+      description:
+        "React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right ...",
+      category: "Developer"
     },
     {
       title: "Redux",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://redux.js.org/",
+      description:
+        "Redux is a predictable state container for JavaScript apps. (Not to be confused with a WordPress framework – Redux Framework.) It helps you write applications ...",
+      category: "Developer"
     },
     {
       title: "JSX",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://facebook.github.io/jsx/",
+      description:
+        "JSX is an XML-like syntax extension to ECMAScript without any defined semantics. It's NOT intended to be implemented by engines or browsers. It's NOT a ...",
+      category: "Developer"
     },
     {
       title: "Angular",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/Angular_(application_platform)",
+      description:
+        "Angular is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and ...",
+      category: "Developer"
     },
     {
       title: "Typescript",
-      url: "",
-			description: "",
-			category: "Developer"
-    },
-    {
-      title: "SCSS",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.typescriptlang.org/",
+      description:
+        "TypeScript brings you optional static type-checking along with the latest ECMAScript features.",
+      category: "Developer"
     },
     {
       title: "Sass",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/Sass_(stylesheet_language)",
+      description:
+        "Sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS). SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, called 'the indented syntax', uses a syntax similar to Haml.",
+      category: "Developer"
+    },
+    {
+      title: "Less",
+      url: "https://en.wikipedia.org/wiki/Less_(stylesheet_language)",
+      description:
+        "Less is a dynamic preprocessor style sheet language that can be compiled into Cascading Style Sheets (CSS) and run on the client side or server side.",
+      category: "Developer"
     },
     {
       title: "Bootstrap",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://getbootstrap.com/",
+      description:
+        "Bootstrap is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites. Bootstrap is completely free to download ...",
+      category: "Developer"
     },
     {
       title: "Materialize",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://materializecss.com/",
+      description:
+        "Materialize is a modern responsive CSS framework based on Material Design by Google.",
+      category: "Developer"
     },
     {
       title: "SQL",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.techopedia.com/definition/1243/sql-server",
+      description:
+        "SQL Server is Microsoft's relational database management system (RDBMS). It is a full-featured database primarily designed to compete against competitors Oracle Database (DB) and MySQL. ... ",
+      category: "Developer"
     },
     {
       title: "PostreSQL",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/PostgreSQL",
+      description:
+        "PostgreSQL, often simply Postgres, is an object-relational database management system (ORDBMS) with an emphasis on extensibility and standards ...",
+      category: "Developer"
     },
     {
       title: "NoSQL",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/NoSQL",
+      description:
+        'A NoSQL (originally referring to "non SQL" or "non relational") database provides a mechanism for storage and retrieval of data that is modeled in means other than the tabular relations used in relational databases.',
+      category: "Developer"
     },
     {
-      title: "Mongo db",
-      url: "",
-			description: "",
-			category: "Developer"
+      title: "MongoDB",
+      url: "https://en.wikipedia.org/wiki/MongoDB",
+      description:
+        "MongoDB is a free and open-source cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like ...",
+      category: "Developer"
     },
     {
       title: "JSON",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.json.org/",
+      description:
+        "JSON (JavaScript Object Notation) is a lightweight data-interchange format. It is easy for humans to read and write. It is easy for machines to parse and generate.",
+      category: "Developer"
     },
     {
       title: "XML",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/XML",
+      description:
+        "Extensible Markup Language (XML) is a markup language that defines a set of rules for encoding documents in a format that is both human-readable and machine-readable.",
+      category: "Developer"
     },
     {
       title: "XSLT",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.w3schools.com/xml/xsl_intro.asp",
+      description:
+        "XSL (eXtensible Stylesheet Language) is a styling language for XML. XSLT stands for XSL Transformations. This tutorial will teach you how to use XSLT to transform XML documents into other formats (like transforming XML into HTML).",
+      category: "Developer"
     },
     {
       title: "Akamai",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.akamai.com/",
+      description:
+        "Akamai is the leading content delivery network (CDN) services provider for media and software delivery, and cloud security solutions.",
+      category: "Developer"
     },
     {
       title: "Restful APIs",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://searchmicroservices.techtarget.com/definition/RESTful-API",
+      description:
+        "A RESTful API is an application program interface (API) that uses HTTP requests to GET, PUT, POST and DELETE data.",
+      category: "Developer"
     },
     {
-      title: "SPA",
-      url: "",
-			description: "",
-			category: "Developer"
+      title: "SPA Single-Page-Application",
+      url:
+        "https://medium.com/@pshrmn/demystifying-single-page-applications-3068d0555d46",
+      description:
+        "A single-page application (SPA) is a website that re-renders its content in response to navigation actions (e.g. clicking a link) without making a ...",
+      category: "Developer"
     },
     {
       title: "GIT",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://git-scm.com/",
+      description:
+        "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency. Git is easy to learn and has a tiny footprint with lightning fast performance.",
+      category: "Developer"
     },
     {
       title: "WordPress",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://wordpress.org/",
+      description:
+        "WordPress is a free and open-source content management system based on PHP and MySQL. Features include a plugin architecture and a template system.",
+      category: "Developer"
     },
     {
       title: "Command Line",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/Command-line_interface",
+      description:
+        "The command line is a text interface for your computer. It's a program that takes in commands, which it passes on to the computer's operating system to run.",
+      category: "Developer"
     },
     {
-      title: "NPM",
-      url: "",
-			description: "",
-			category: "Developer"
+      title: "npm",
+      url: "https://www.npmjs.com/",
+      description:
+        "npm is the package manager for JavaScript and the world's largest software registry. Discover packages of reusable code — and assemble them in powerful ...",
+      category: "Developer"
     },
     {
       title: "Agile",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/Agile_software_development",
+      description:
+        "Agile software development is an approach to software development under which requirements and solutions evolve through the collaborative effort of ...",
+      category: "Developer"
     },
     {
       title: "Github",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://github.com/",
+      description:
+        "GitHub Inc. is a web-based hosting service for version control using Git. It is mostly used for computer code. It offers all of the distributed version control and source code management functionality of Git as well as adding its own features.",
+      category: "Developer"
     },
     {
       title: "Source Tree",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.sourcetreeapp.com/",
+      description:
+        "A Git GUI that offers a visual representation of your repositories. Sourcetree is a free Git client for Windows and Mac.",
+      category: "Developer"
     },
     {
       title: "Jira",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.atlassian.com/software/jira",
+      description:
+        "Jira is a proprietary issue tracking product developed by Atlassian which allows bug tracking and agile project management.",
+      category: "Developer"
     },
     {
       title: "Bitbucket",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://bitbucket.org/",
+      description:
+        "Bitbucket is a web-based version control repository hosting service owned by Atlassian, for source code and development projects that use either Mercurial or Git revision control systems.",
+      category: "Developer"
     },
     {
       title: "Confluence",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://www.atlassian.com/software/confluence",
+      description:
+        "Confluence is a collaboration software program developed and published by Australian software company Atlassian.",
+      category: "Developer"
     },
     {
       title: "email",
-      url: "",
-			description: "",
-			category: "Developer"
+      url: "https://en.wikipedia.org/wiki/Email",
+      description:
+        "E-mail (electronic mail) is the exchange of computer-stored messages by telecommunication. (Some publications spell it email; we prefer the currently more established spelling of e-mail.) E-mail messages are usually encoded in ASCII text. ... ",
+      category: "Developer"
+    },
+    {
+			title: "Marketing Strategy",
+			url: "https://en.wikipedia.org/wiki/Marketing_strategy",
+			description: "Marketing strategy is a long-term, forward-looking approach to planning with the fundamental goal achieving a sustainable competitive advantage.",
+      category: "Business"
+    },
+    {
+			title: "Online Advertising",
+			url: "https://www.wordstream.com/online-advertising",
+			description: "Online advertising is one of the most effective ways for businesses of all sizes to expand their reach, find new customers, and diversify their revenue streams.",
+      category: "Business"
+    },
+    {
+			title: "SEO Search Engine Optimization",
+			url: "https://en.wikipedia.org/wiki/Search_engine_optimization",
+			description: "SEO is a marketing discipline focused on growing visibility in organic (non-paid) search engine results. SEO encompasses both the technical and creative elements required to improve rankings, drive traffic, and increase awareness in search engines.",
+      category: "Business"
+    },
+    {
+			title: "Social Media Marketing",
+			url: "https://www.wordstream.com/social-media-marketing",
+			description: "Social media marketing, or SMM, is a form of internet marketing that involves creating and sharing content on social media networks in order to achieve your marketing and branding goals.",
+      category: "Business"
+    },
+    {
+			title: "email Marketing",
+			url: "https://en.wikipedia.org/wiki/Email_marketing",
+			description: "Email marketing is the highly effective digital marketing strategy of sending emails to prospects and customers. Effective email marketing converts prospects into customers, and turns one-time buyers into loyal, raving fans.",
+      category: "Business"
+    },
+    {
+			title: "Digital Media Planning",
+			url: "https://www.mediaocean.com/digital-marketing-guide/media-planning-buying",
+			description: "Media planning is the process of strategically selecting a mix of media platforms to place ads over a period of time in order to achieve an advertiser's campaign goals.",
+      category: "Business"
+    },
+    {
+      title: "English",
+			url: "https://en.wikipedia.org/wiki/English_language",
+			description: "English is a West Germanic language that was first spoken in early medieval England and is now a global lingua franca.",
+      category: "Language"
+    },
+    {
+      title: "Spanish",
+			url: "https://en.wikipedia.org/wiki/Spanish_language",
+			description: "Spanish or Castilian, is a Western Romance language that originated in the Castile region of Spain and today has hundreds of millions of native speakers in the Americas and Spain.",
+			category: "Language"
     }
-  ],
+  ]
 };
