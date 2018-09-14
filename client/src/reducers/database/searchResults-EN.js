bb1cacd35e1feab4a7d715eee3fbd38c06a9e530
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-13 20:28:34 -0700

    experience added to bd

--- client/src/reducers/database/searchResults-EN.js
+++ client/src/reducers/database/searchResults-EN.js
@@ -1,5 +1,6 @@
 import skills from "./skills";
+import experience from "./experience"
 
 export default [
-	skills/*, "experience", "projects", "about", "education", "interests", "awards", "references", "social media"*/
+	skills, experience/*, "projects", "about", "education", "interests", "awards", "references", "social media"*/
 ]
