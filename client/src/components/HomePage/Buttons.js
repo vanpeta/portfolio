fe0fa78e6a9be692921b3785630de3d414b74cfc
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-18 19:21:34 -0700

    adding functionality to homepage buttons

--- client/src/components/HomePage/Buttons.js
+++ client/src/components/HomePage/Buttons.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { contacting } from "../../actions/index";
 import "./styles/Buttons.css";
 
 class Buttons extends Component {
+  constructor(props) {
+    super(props);
+    this.openContact = this.openContact.bind(this);
+  }
+
+  openContact() {
+    this.props.contacting(true);
+  }
+
   render() {
     return (
       <div className="ButtonsBox">
         <div className="btn">Carlos Search</div>
-				<div className="btn">I'm Feeling Lucky</div>
+        <div className="btn" onClick={this.openContact}>
+          I'm Feeling Lucky
+        </div>
       </div>
     );
   }
 }
 
-export default Buttons;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      contacting
+    },
+    dispatch
+  )
+}
+
+export default connect(null, mapDispatchToProps)(Buttons);
