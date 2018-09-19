d9fc80ad2b89bb8728c348cc90e64e36c9e30699
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-19 11:13:57 -0700

    adding education

---
+++ client/src/reducers/database/education.js
@@ -0,0 +1,104 @@
+export default {
+	searchTerms: ["education", "what did you study", "college", "high school", "university"],
+	about: "life stages in (0.37 century)",
+	results: [
+		{
+			title: "Bachelor of Arts in Advertising and Public Relations",
+			url: "",
+			description: "",
+			category: "Advanced",
+			links: [
+				{
+					text: "University of Valladolid",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "Web Development Boot Camp",
+			url: "",
+			description: "",
+			category: "Advanced",
+			links: [
+				{
+					text: "General Assembly",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "Business Administration Program",
+			url: "",
+			description: "",
+			category: "Advanced",
+			links: [
+				{
+					text: "Universidad Francisco de Vitoria",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "High School speciality in Science",
+			url: "",
+			description: "",
+			category: "Traditional",
+			links: [
+				{
+					text: "Colegio Hijas de Jesus",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "Social Media Marketing Course",
+			url: "",
+			description: "",
+			category: "Optional",
+			links: [
+				{
+					text: "Mediabristo.com",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "International Relations and Protocol",
+			url: "",
+			description: "",
+			category: "Optional",
+			links: [
+				{
+					text: "Escuela internacional de diplomacia",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "Music Theory",
+			url: "",
+			description: "",
+			category: "Optional",
+			links: [
+				{
+					text: "Conservatorio de Música de Medina del Campo",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		}
+	]
+};
--- client/src/reducers/database/searchResults-EN.js
+++ client/src/reducers/database/searchResults-EN.js
@@ -1,6 +1,7 @@
 import skills from "./skills";
 import experience from "./experience"
 import about from "./about";
+import education from "./education";
 export default [
-  experience, skills, about/*, "projects", "about", "education", "interests", "awards", "references", "social media"*/
+  experience, skills, about, education/*, "projects", "about", "interests", "awards", "references", "social media"*/
 ];
