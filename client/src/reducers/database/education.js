bb418e7c862ef28dccecd048a9cc2af88c111c46
Carlos Allende <carlosallendesanz@gmail.com>
2018-09-19 20:49:05 -0700

    adding content to education

--- client/src/reducers/database/education.js
+++ client/src/reducers/database/education.js
@@ -1,15 +1,15 @@
 export default {
 	searchTerms: ["education", "what did you study", "college", "high school", "university"],
-	about: "life stages in (0.37 century)",
+	about: "main educational stages in (0.37 century) and counting.",
 	results: [
 		{
-			title: "Bachelor of Arts in Advertising and Public Relations",
+			title: "Bachelor of Arts in Communication Sciences - Advertising and Public Relations",
 			url: "",
-			description: "",
+			description: "5 years college degree focused on communication sciences. Innovetion, trends, technology, persuasion, ethics and marketing are just some of the disciplines this degree developes in deep.",
 			category: "Advanced",
 			links: [
 				{
-					text: "University of Valladolid",
+					text: "Universidad de Valladolid",
 					image: "",
 					mapLocation: { lat: "", lng: "" },
 					description: ""
@@ -17,9 +17,9 @@ export default {
 			]
 		},
 		{
-			title: "Web Development Boot Camp",
-			url: "",
-			description: "",
+			title: "Web Development Inmersive Program",
+			url: "https://generalassemb.ly/education/web-development-immersive",
+			description: "A 6 months intensive program with focus on computer science and web development. It touches the main areas a full stack developer must know. Back-end technologies like Ruby on Rails and Node.js, databases SQL (postgresql) and noSQL (mongodb) and modern front-end javascript frameworks like Angular or React and team collaboration strategies are some of the skills taught.",
 			category: "Advanced",
 			links: [
 				{
@@ -32,8 +32,8 @@ export default {
 		},
 		{
 			title: "Business Administration Program",
-			url: "",
-			description: "",
+			url: "http://www.mastermas.com/Reportajes/P1.asp?Reportaje=936",
+			description: "This program focuses on developing bussiness skills for college graduates that were awarded with the Optimus Scholarship upon graduation. The program makes compatible a working intership with academic lectures during a period of 18 months. It focuses on bussines ethics, global bussines vision, anthropology, human behavior, negotiation techniques, team working, motivation and more.",
 			category: "Advanced",
 			links: [
 				{
@@ -45,27 +45,13 @@ export default {
 			]
 		},
 		{
-			title: "High School speciality in Science",
-			url: "",
-			description: "",
-			category: "Traditional",
-			links: [
-				{
-					text: "Colegio Hijas de Jesus",
-					image: "",
-					mapLocation: { lat: "", lng: "" },
-					description: ""
-				}
-			]
-		},
-		{
-			title: "Social Media Marketing Course",
-			url: "",
-			description: "",
+			title: "Digital Marketing Course",
+			url: "https://www.mediabistro.com",
+			description: "Mediabistro is a website that offers resources for media professionals. It publishes various blogs which analyze the mass media industry. The program focused on digital marketing techniques like planning, strategy, execution and analytics of digital marketing campaigns.",
 			category: "Optional",
 			links: [
 				{
-					text: "Mediabristo.com",
+					text: "Mediabristo",
 					image: "",
 					mapLocation: { lat: "", lng: "" },
 					description: ""
@@ -88,12 +74,26 @@ export default {
 		},
 		{
 			title: "Music Theory",
-			url: "",
-			description: "",
+			url: "https://en.wikipedia.org/wiki/Music_theory",
+			description: "Music theory is the study of the practices and possibilities of music.",
 			category: "Optional",
 			links: [
 				{
 					text: "Conservatorio de Música de Medina del Campo",
+					image: "",
+					mapLocation: { lat: "", lng: "" },
+					description: ""
+				}
+			]
+		},
+		{
+			title: "High School speciality in Science",
+			url: "",
+			description: "4 years with a focus on subjects like chemistry, physics, technical drawing, algebra, calculus.",
+			category: "Traditional",
+			links: [
+				{
+					text: "Colegio Hijas de Jesus",
 					image: "",
 					mapLocation: { lat: "", lng: "" },
 					description: ""
